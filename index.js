5033addb49fb2127fc33c692cc0ede69d20d2220
pravin116 <50068556+pravin116@users.noreply.github.com>
2019-11-28 18:55:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var x = myFunction(2, 3);   // Function is called, return value will end up in x
+console.log(x);
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
